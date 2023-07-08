ac6d3a5400ef0e6684b8c608443c9bbd6bdb7751
Matt <matt@silbernagel.dev>
2023-07-08 15:16:25 -0400

    Make credo happy

--- lib/beamring_web/components/layouts.ex
+++ lib/beamring_web/components/layouts.ex
@@ -1,4 +1,6 @@
 defmodule BeamringWeb.Layouts do
+  @moduledoc false
+
   use BeamringWeb, :html
 
   embed_templates "layouts/*"
--- lib/beamring_web/telemetry.ex
+++ lib/beamring_web/telemetry.ex
@@ -1,4 +1,5 @@
 defmodule BeamringWeb.Telemetry do
+  @moduledoc false
   use Supervisor
   import Telemetry.Metrics
 
